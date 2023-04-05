76f4104277cf47955287278d74eb303bb026d771
Alessandro <afalcione@archimede.lngs.infn.it>
2023-04-05 15:00:04 +0200

    Prova git auto

--- scripts/dailyRun.sh
+++ scripts/dailyRun.sh
@@ -17,50 +17,45 @@ HourEnd=00
 
 echo 'Start: ' $YearStart $MonthStart $DayStart $HourStart 'End: ' $YearEnd $MonthEnd $DayEnd $HourEnd
 
+# Download dati
 cd ${HOME}/DATA
 rm -f gfs*
 rm -r wget-log*
 ./downloadDataGFS.sh $YearStart $MonthStart $DayStart $HourStart
 
+# Namelist
 cd ${HOME}
 ./writeNamelistWPS.sh $YearStart $MonthStart $DayStart $HourStart $YearEnd $MonthEnd $DayEnd $HourEnd
-
 ./writeNamelistInput.sh $YearStart $MonthStart $DayStart $HourStart $YearEnd $MonthEnd $DayEnd $HourEnd
 
+# WPS
 cd ${HOME}/WRF/WPSv4.4
-
 ./geogrid.exe
-
 rm -f GRIBFILE* 
 ./link_grib.csh ${HOME}/DATA/gfs*
-
 rm -f FILE*
 ./ungrib.exe
-
 rm -f met_em*
 ./metgrid.exe
-
 cd ${HOME}/WRF/WRFv4.4.2/run
-
 rm -f met_em*
 ln -s ${HOME}/WRF/WPSv4.4/met_em* .
 
+# WRF
 rm -f wrfinput*
 rm -f wrfbdy*
-mpirun -np 100 ./real.exe
-
+mpirun -np 64 ./real.exe
 rm -f wrfout*
-#/opt/intel/oneapi/mpi/2021.8.0/bin/mpirun
-mpirun -np 100 ./wrf.exe
-
-
+mpirun -np 64 ./wrf.exe
 rm -f ${HOME}/out/wrfout*
 mv wrfout* ${HOME}/out
 
+# Plot
 rm -f ${HOME}/plots/*.jpg
 cd ${HOME}/plots
 ./plotParallel.sh "${HourStart}:00:00 ${YearStart}-${MonthStart}-${DayStart}"
 
+# Git
 cd ${HOME}
 ./gitUpdate.sh
 
